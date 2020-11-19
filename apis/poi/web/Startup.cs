b73c109594b457e4f45b63cb5d29b43ccb187c58
Ben Magazino <bemagazi@microsoft.com>
2020-11-19 14:00:47 -0600

    Added POI app insights

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -30,6 +30,8 @@ namespace poi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
+
             services.AddMvc()
                 .AddJsonOptions(options =>
                 {
